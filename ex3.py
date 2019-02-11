f47e3c45ea9cc18288e0c633a4ef6cb459e94d62
Dante <andi.etnad@gmail.com>
2019-02-11 19:53:48 +0200

    first commit

---
+++ ex3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# 11:30 
+# Using print statement to output a string to the screen.
+print("I will now count my chickens: ")
+# Use print statement to output math
+print("Hens", 25 + 30 / 6)
+
+print("Now I will count the eggs: ")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("It is true that 3 + 2 < 5 -7 ?")
+print(3 + 2 < 5 -7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more?")
+
+print("Is it greater?", 5 > -2)
+print("Is is greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
---
+++ ex8.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+"Try your",
+"Own text here",
+"Maybea poem",
+"Or a song about fear"
+))
